4ddfcedfd36a7c25c4c9bec43f5fdcf61b2b02c9
Preksha Gupta <prekshagupta03@gmail.com>
2024-02-19 13:47:01 +0530

    pincode delivery app used for ordering food worldwide

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import User from "../components/User";
+
+class About extends React.Component {
+    render() {
+        return (
+            <User />
+        )
+    }
+}
+
+export default About;
